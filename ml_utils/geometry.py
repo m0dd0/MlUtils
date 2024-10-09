2d01c59ccfb49b054578594889bedac61c0e7557
Moritz Hesche <moritz.hesche@zeiss.com>
2024-10-09 17:15:09 +0200

    refactor PositionRotation2Pose to position_rotation2pose function

--- ml_utils/geometry.py
+++ ml_utils/geometry.py
@@ -4,24 +4,23 @@ import numpy as np
 from ml_utils.array_typing import NpArray
 
 
-class PositionRotation2Pose:
-    def __call__(
-        self, positions: NpArray["n,3", float], rotations: NpArray["n,3,3", float]
-    ) -> NpArray["n,4,4", float]:
-        batched_input = len(positions.shape) == 2
+def position_rotation2pose(
+    positions: NpArray["n,3", float], rotations: NpArray["n,3,3", float]
+) -> NpArray["n,4,4", float]:
+    batched_input = len(positions.shape) == 2
 
-        if not batched_input:
-            position = position[None]
-            rotations = rotations[None]
+    if not batched_input:
+        position = position[None]
+        rotations = rotations[None]
 
-        n_poses = position.shape[0]
+    n_poses = position.shape[0]
 
-        poses = np.zeros((n_poses, 4, 4), dtype=float)
-        poses[:, :3, :3] = rotations
-        poses[:, :3, 3] = position
-        poses[:, 3, 3] = 1
+    poses = np.zeros((n_poses, 4, 4), dtype=float)
+    poses[:, :3, :3] = rotations
+    poses[:, :3, 3] = position
+    poses[:, 3, 3] = 1
 
-        if not batched_input:
-            poses = poses[0]
+    if not batched_input:
+        poses = poses[0]
 
-        return poses
+    return poses
